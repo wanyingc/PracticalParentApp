09edcb58765e9413f4b0a145fd5307a97ba52d2b
gta23 <gage_tylee@sfu.ca>
2020-10-31 15:38:57 -0700

    Created CoinFlipActivity  - Added layout for coin flip

---
+++ app/src/main/java/ca/cmpt276/practicalparent/CoinFlipActivity.java
@@ -0,0 +1,24 @@
+package ca.cmpt276.practicalparent;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class CoinFlipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin_flip);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+}
